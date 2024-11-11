7a3a12700d2a9142bb6643909466fad11120b3eb
Nasif Shuvo <shuvo.nasif@gmail.com>
2024-11-11 15:38:49 +0600

    re-server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const handleRasaMessage = require('./rasaWorker');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
   methods: ['GET', 'POST'],
   credentials: true
 }));
@@ -21,7 +21,7 @@ app.use(cors({
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
     methods: ['GET', 'POST'],
     credentials: true
   }
